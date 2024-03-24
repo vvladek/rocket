f0c3e07dac16dd6655dc21422ab731d5f8b5ba9a
Wladek <wladek.developer@gmail.com>
2024-03-24 19:56:01 +0100

    refactor: change rocket html structure

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
+let isPossibleClick = true
+
 document.querySelector(".rocket").addEventListener("click", (event) => {
-    
+    if (isPossibleClick) {
+        isPossibleClick = false
+        event.target.style.transform = "none";
+        setTimeout(() => {
+            isPossibleClick = true
+        }, 1000)
+    }
+})
+
+document.querySelector("button").addEventListener("click", () => {
+    document.querySelector(".second__stage").classList.toggle("flame")
+    document.querySelector(".first__stage__left").classList.toggle("flame")
+    document.querySelector(".first__stage__right").classList.toggle("flame")
 })
