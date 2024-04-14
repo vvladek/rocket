77c6862779d9ae67d67431817293d3d97a8a29e6
Wladek <wladek.developer@gmail.com>
2024-04-14 13:53:02 +0200

    fix: change vite config

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ import { VitePWA } from "vite-plugin-pwa"
 export default {
     plugins: [
         VitePWA({
-            registerType: "autoUpdate"
+            registerType: "autoUpdate",
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg,webp,mp3,ogg}']
+            }
         })
     ],
-    workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp,mp3,ogg}'],
-    }
 }
